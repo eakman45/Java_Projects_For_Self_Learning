6760eada8b82e6fa2cf82ba228f58a7f3737d0a8
omera <ege.akman1305@gmail.com>
2023-01-20 17:54:24 +0300

    n to the power of r(for/while)

--- src/rpowerofn.java
+++ src/rpowerofn.java
@@ -9,14 +9,17 @@ public class rpowerofn {
         System.out.println("Exponential");
         int r = scan.nextInt();
 
-        int counter = 1;
+        //int counter = 1;
         int total = 1;
 
         //3^4 = 3 * 3 * 3 * 3;
 
-        while(counter <= r){
+        /*while(counter <= r){
             total=total * n;
             counter++;
+        }*/
+        for(int i =1;i <=r ;i++){
+            total=total*n;
         }
 
         System.out.println(n+" to the power of "+r+" is = "+total);
