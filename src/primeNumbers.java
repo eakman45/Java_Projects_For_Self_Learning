73bd3f0e5a9f26f759bb5c5c19d708d2b19eef55
omera <ege.akman1305@gmail.com>
2023-02-20 21:57:51 +0300

    prime numbers.

---
+++ src/primeNumbers.java
@@ -0,0 +1,20 @@
+public class primeNumbers {
+    public static void main(String[] args) {
+
+        int counter=0;
+
+        for (int i = 1; i < 100; i++) {
+            for(int j=1;j<=i;j++){
+                if(i%j==0){
+                    counter++;
+                }
+
+            }
+            if(counter==2){
+                System.out.print(" "+i);
+            }
+            counter=0;
+        }
+    }
+}
+
