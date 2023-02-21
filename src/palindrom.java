2621e5e9ccd538f1890568d47d7c796f62686e10
omera <ege.akman1305@gmail.com>
2023-02-22 02:34:23 +0300

    palindrom number check.

---
+++ src/palindrom.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class palindrom {
+    static boolean isPalindrom(int number){
+
+        int temp=number,reverseNumber=0,lastDecimal;
+        while(temp != 0){
+            //To get the last digit of the number:
+            lastDecimal = temp % 10;
+            /*
+            Using reverse number we can reverse the entered number.
+            */
+            reverseNumber = (reverseNumber*10)+lastDecimal;
+            //to last digit.
+            temp /= 10;
+        }
+        return reverseNumber == number;
+    }
+    public static void main(String[] args) {
+        Scanner myInput = new Scanner(System.in);
+        int n= myInput.nextInt();
+        System.out.println(isPalindrom(n));
+
+    }
+}
